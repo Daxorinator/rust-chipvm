40ede8b622edf3b7d96b4547a365ae5f3c6ea2e6
Seán Kelly <daxorinator@gmail.com>
2019-04-04 14:01:34 +0100

    Added src and cargo files

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+}
+
+static mut Memory: [u8; 4096] = [0; 4096];
+
+static mut Stack: [u16, 16] = [0; 16];
+
+static mut Display: [bool, 64][bool, 32] = [false; 64][false; 32];
+
+struct Registers {
+    i: u16,
+    pc: u16,
+    sp: u16,
+    dt: u8,
+    st: u8,
+    
+    v0: u8,
+    v1: u8,
+    v2: u8,
+    v3: u8,
+    v4: u8,
+    v5: u8,
+    v6: u8,
+    v7: u8,
+    v8: u8,
+    v9: u8,
+    va: u8,
+    vb: u8,
+    vc: u8,
+    vd: u8,
+    ve: u8,
+    vf: u8
+}
