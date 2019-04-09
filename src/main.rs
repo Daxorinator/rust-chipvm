26c08f25aee24ed9980a791447c1fe063d0ceada
Seán Kelly <daxorinator@gmail.com>
2019-04-09 18:41:48 +0100

    Added chip8.rs and implemented basic file read

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,13 @@
+mod chip8;
+use std::fs;
+
 fn main() {
-}
+    let mut registers: chip8::Registers = Default::default();
+    let mut STACK: [u16; 16] = [0x000000000000000; 16];
+    let mut DISPLAY: [[bool; 64]; 32] = [[false; 64]; 32];
+    let mut MEMORY: Vec<u8> = fs::read("rom").unwrap();
 
-static mut Memory: [u8; 4096] = [0; 4096];
 
-static mut Stack: [u16, 16] = [0; 16];
-
-static mut Display: [bool, 64][bool, 32] = [false; 64][false; 32];
-
-struct Registers {
-    i: u16,
-    pc: u16,
-    sp: u16,
-    dt: u8,
-    st: u8,
     
-    v0: u8,
-    v1: u8,
-    v2: u8,
-    v3: u8,
-    v4: u8,
-    v5: u8,
-    v6: u8,
-    v7: u8,
-    v8: u8,
-    v9: u8,
-    va: u8,
-    vb: u8,
-    vc: u8,
-    vd: u8,
-    ve: u8,
-    vf: u8
+
 }
