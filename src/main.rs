3ce6e56b1a050d84e69a05325a57bac896009728
Seán Kelly <daxorinator@gmail.com>
2019-04-10 20:35:08 +0100

    Finished file loading, starting to parse instructions

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,24 @@ use std::fs;
 
 fn main() {
     let mut registers: chip8::Registers = Default::default();
-    let mut STACK: [u16; 16] = [0x000000000000000; 16];
-    let mut DISPLAY: [[bool; 64]; 32] = [[false; 64]; 32];
-    let mut MEMORY: Vec<u8> = fs::read("rom").unwrap();
+    let mut stack: [u16; 16] = [0x000000000000000; 16];
+    let mut display: [[bool; 64]; 32] = [[false; 64]; 32];
+    let memory = fs::read("rom").unwrap();
+
+    for bytes in memory.chunks(2) {
+        let h1 = bytes[0] >> 4;
+        let l1 = bytes[0] & 15;
+
+        let h2 = bytes[1] >> 4;
+        let l2 = bytes[1] & 15;
+
+        match h1 {
+            0x0 => match l2 {
+                0x0 => cls(),
+                0xE => ret(),
+                _ => println!("Not an instruction")
+            }
+            _ => println!("Go back and implement {:x?}", h1)
+        }
+    }
 }
