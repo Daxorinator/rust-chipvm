7f2a94d11020a7de3ee4a30c41eec8a31dd7979f
Seán Kelly <daxorinator@gmail.com>
2019-04-11 16:27:22 +0100

    Finished a reworked instruction match

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,77 @@ fn main() {
         let h2 = bytes[1] >> 4;
         let l2 = bytes[1] & 15;
 
-        match h1 {
-            0x0 => match l2 {
-                0x0 => cls(),
-                0xE => ret(),
-                _ => println!("Not an instruction")
-            }
-            _ => println!("Go back and implement {:x?}", h1)
+        match (h1, l1, h2, l2) {
+            
+            (0x0, 0x0, 0xE, 0x0) => cls(),
+
+            (0x0, 0x0, 0xE, 0xE) => ret(),
+
+            (0x1,  _ ,  _ ,  _ ) => jmp(l1, h2, l2),
+
+            (0x2,  _ ,  _ ,  _ ) => call(l1, h2, l2),
+
+            (0x3,  _ ,  _ ,  _ ) => se(l1, h2, l2),
+
+            (0x4,  _ ,  _ ,  _ ) => sne(l1, h2, l2),
+
+            (0x5,  _ ,  _ , 0x0) => se(l1, h2),
+
+            (0x6,  _ ,  _ ,  _ ) => ld(l1, h2, l2),
+
+            (0x7,  _ ,  _ ,  _ ) => add(l1, h2, l2),
+
+            (0x8,  _ ,  _ , 0x0) => ld(l1, h2),
+
+            (0x8,  _ ,  _ , 0x1) => or(l1, h2),
+
+            (0x8,  _ ,  _ , 0x2) => and(l1, h2),
+
+            (0x8,  _ ,  _ , 0x3) => xor(l1, h2),
+
+            (0x8,  _ ,  _ , 0x4) => add(l1, h2),
+
+            (0x8,  _ ,  _ , 0x5) => sub(l1, h2),
+
+            (0x8,  _ ,  _ , 0x6) => shr(l1, h2),
+
+            (0x8,  _ ,  _ , 0x7) => subn(l1, h2),
+
+            (0x8,  _ ,  _ , 0xE) => shl(l1, h2),
+
+            (0x9,  _ ,  _ , 0x0) => sne(l1, h2),
+
+            (0xA,  _ ,  _ ,  _ ) => ld(l1, h2, l2),
+
+            (0xB,  _ ,  _ ,  _ ) => jp(l1, h2, l2),
+
+            (0xC,  _ ,  _ ,  _ ) => rnd(l1, h2, l2),
+
+            (0xD,  _ ,  _ ,  _ ) => drw(l1, h2, l2),
+
+            (0xE,  _ , 0x9, 0xE) => skp(l1),
+
+            (0xE,  _ , 0xA, 0x1) => sknp(l1),
+
+            (0xF,  _ , 0x0, 0x7) => ld(l1, dt),
+
+            (0xF,  _ , 0x0, 0xA) => ld(l1, key_press),
+
+            (0xF,  _ , 0x1, 0x5) => ld(dt, l1),
+
+            (0xF,  _ , 0x1, 0x8) => ld(st, l1),
+
+            (0xF,  _ , 0x1, 0xE) => add(dt, l1),
+
+            (0xF,  _ , 0x2, 0x9) => ,
+
+            (0xF,  _ , 0x3, 0x3) => ,
+
+            (0xF,  _ , 0x5, 0x5) => ,
+
+            (0xF,  _ , 0x6, 0x5) => ,
+            
+            _ => println!("{:x?} is not an instruction", [h1, l1, h2, l2])
         }
     }
 }
